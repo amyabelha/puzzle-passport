ecbdbf69d7f5bd4535c5e66e2c08291f239837e9
Amy <66322971+amyabelha@users.noreply.github.com>
2024-02-22 09:52:24 -0700

    hello world
    add template index, css, js files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('searchPuzzle').addEventListener('click', function() {
+    window.open('searchPuzzle.html', '_blank');
+});
+
+document.getElementById('registerPuzzle').addEventListener('click', function() {
+    alert('Register Puzzle clicked!');
+});
