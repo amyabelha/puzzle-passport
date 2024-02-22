e5ee69db77a1710dae07b6af0d7f746ae4120133
Amy <66322971+amyabelha@users.noreply.github.com>
2024-02-22 10:40:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var baseUrl = window.location.origin + window.location.pathname;
+document.getElementById('searchPuzzle').addEventListener('click', function() {
+    window.open('searchPuzzle.html', '_self');
+});
 
-    // Ensure baseUrl ends with a slash
-    if (!baseUrl.endsWith('/')) {
-        baseUrl += 'https://github.com/amyabelha/puzzle_passport/blob/main/';
-    }
-
-    document.getElementById('searchPuzzle').addEventListener('click', function() {
-        window.open(baseUrl + 'searchPuzzle.html', '_blank');
-    });
-
-    document.getElementById('registerPuzzle').addEventListener('click', function() {
-        window.open(baseUrl + 'registerPuzzle.html', '_blank');
-    });
+document.getElementById('registerPuzzle').addEventListener('click', function() {
+    window.open('registerPuzzle.html', '_self');
 });
