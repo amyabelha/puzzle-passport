42b71d7ad8801f57d129f1d56c6b5d45d882a4da
Amy <66322971+amyabelha@users.noreply.github.com>
2024-02-22 10:25:56 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('searchPuzzle').addEventListener('click', function() {
-     a href="searchPuzzle.html" onclick="window.open('searchPuzzle.html', '_self');
-});
-
-document.getElementById('registerPuzzle').addEventListener('click', function() {
-    alert('Register Puzzle clicked!');
-});
